9530d2ebc77f2e2421e0e436a2d7658ee1a96122
jenadiusnicholaus <ucode888@gmail.com>
2022-03-21 19:35:53 +0300

    fixs

--- app.js
+++ app.js
@@ -1,68 +1,64 @@
-// books = [
 
-// ]
-
-// const add = () => {
-//     ob = {
-//         a: 'jena',
-//         b: 'jeb',
-//     }
-
-//    books.push(ob);
-
-//    od = {
-//     a: 'jena',
-//     b: 'jeb',
-//    }
-//    books.push( od);
-//    oc = {
-//     a: 'jena',
-//     b: 'jeb',
-//    }
-//     books.push( oc);
-// }
-   
-    
-
-// // on button click event
-// add();
-
-// localStorage.setItem("books", JSON.stringify(books));
-
-// // on window load clist all bools created
-
-
-// var storedNames = JSON.parse(localStorage.getItem("books"));
-
-
-// // console. log(books)
-
-// window.addEventListener('load', () => {
- 
-//     storedNames.map(obj => {
-//         console.log(obj)
-//     })
-// })
-
-const titleEl = document.getElementById('title');
-const authorEl = document.getElementById('author');
 let books = []
 
-document.getElementById('addbtn').addEventListener('click', () => {
-
-     class bookList {
-        constructor(title, author) {
-            this.title = title;
-            this.author = author;
-        }
+class Book{
+    constructor(title, author){
+        this.title = title;
+        this.author = author
     }
+}
 
-    let book = new bookList(titleEl.value, authorEl.value);
+// helper function to get information
+function getBooks(){
+    const storedNames = JSON.parse(localStorage.getItem("books"));
+    const cardContainer = document.getElementById('books');
+    cardContainer.innerHTML = '';
+    let cardElement = '';
 
-    books.push(book);
+    for(let i = 0; i < storedNames.length; i +=1 ) {
+        console.log(storedNames[i])
+        cardElement += `<div class="card">
+            <h2 class="content">${storedNames[i].title}, </h2>
+            <button class='remove' value="${i}" type="button"> Remove</button>
+        </div> <hr>`;
+    }
+   cardContainer.innerHTML = cardElement;
+}
+
+
+document.getElementById('addbtn').addEventListener('click', () => {  
+const titleEl = document.getElementById('title');
+const authorEl = document.getElementById('author');
+books.push(Book(titleEl,value, authorEl.value))
+localStorage.setItem('books', JSON.stringify(books));
+getBooks()
+
+});
+
+console.log(books)
+
+window.addEventListener('load',  () => {
+
+    getBooks()
 
 })
 
+document.getElementById('books').addEventListener('click', (event) => {
+
+    const isButton = event.target.nodeName === 'BUTTON';
+  
+    if (!isButton) {
+      return;
+    }
+    remove(event.target.value , event)
+  });
+
+  function remove(index , e){
+     books.splice(index,1);
+     console.log(books)
+      localStorage.setItem('books', JSON.stringify(books));
+      window.location.reload();
+   }
 
 
 
