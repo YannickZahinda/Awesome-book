061883fe688b1334337f0287a10b006bae9fda2b
jenadiusnicholaus <ucode888@gmail.com>
2022-03-23 11:35:46 +0300

    Update js file

--- app.js
+++ app.js
@@ -3,48 +3,43 @@
 const titleEl = document.getElementById('title');
 const authorEl = document.getElementById('author');
 const cardContainer = document.getElementById('books');
-
 class Book {constructor(title, author) { this.title = title; this.author = author; }}
 
 // utility methods
 class BooksStoreUtilities {
     static getBooks =() => JSON.parse(localStorage.getItem('books') || '[]')
-
     static removeByTitle =(title) => {
       const books = this.getBooks();
       const filteredBooks = books.filter((item) => item.title !== title);
       localStorage.setItem('books', JSON.stringify(filteredBooks));
     }
-
     static addBook = (books) => {
       books.push(new Book(titleEl.value, authorEl.value));
       localStorage.setItem('books', JSON.stringify(books));
-    }
+    }   
 }
 
 // main render methods
 class MainUI {
     static displayBooks = () => {
-      const storedBooks = BooksStoreUtilities.getBooks();
-      cardContainer.innerHTML = '';
-      let cardElement = '';
-
-      for (let i = 0; i < storedBooks.length; i += 1) {
-        cardElement += `<li class="card">
-                <h2 class="content">${storedBooks[i].title}, </h2>
-                <p class="content">${storedBooks[i].author}</p>
-                <button class='remove' value="${storedBooks[i].title}" type="button"> Remove</button>
-            </li><hr/>`;
+        const storedBooks = BooksStoreUtilities.getBooks();
+        cardContainer.innerHTML = '';
+        let cardElement = '';
+  
+        for (let i = 0; i < storedBooks.length; i += 1) {
+          cardElement += `<div class="card">
+                  <h2 class="content">${storedBooks[i].title}, </h2>
+                  <button class='remove' value="${storedBooks[i].title}" type="button"> Remove</button>
+              </div> <hr>`;
+        }
+        cardContainer.innerHTML = cardElement;
       }
-      cardContainer.innerHTML = cardElement;
-    }
 }
 
 // add book
 document.getElementById('addbtn').addEventListener('click', () => {
   const oldBooks = BooksStoreUtilities.getBooks();
-  oldBooks.push(new Book(titleEl.value, authorEl.value));
-  localStorage.setItem('books', JSON.stringify(oldBooks));
+  BooksStoreUtilities.addBook(oldBooks);
   MainUI.displayBooks();
 });
 
@@ -58,5 +53,5 @@ document.getElementById('books').addEventListener('click', (event) => {
 
 //  get all books
 window.addEventListener('load', () => {
-  MainUI.displayBooks();
+    MainUI.displayBooks();
 });
