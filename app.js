c8236a3cfcee7131c930be5ba29378ddd5c4a717
jenadiusnicholaus <ucode888@gmail.com>
2022-03-23 11:44:47 +0300

    fix linters

--- app.js
+++ app.js
@@ -8,32 +8,34 @@ class Book {constructor(title, author) { this.title = title; this.author = autho
 // utility methods
 class BooksStoreUtilities {
     static getBooks =() => JSON.parse(localStorage.getItem('books') || '[]')
+
     static removeByTitle =(title) => {
       const books = this.getBooks();
       const filteredBooks = books.filter((item) => item.title !== title);
       localStorage.setItem('books', JSON.stringify(filteredBooks));
     }
+
     static addBook = (books) => {
       books.push(new Book(titleEl.value, authorEl.value));
       localStorage.setItem('books', JSON.stringify(books));
-    }   
+    }
 }
 
 // main render methods
 class MainUI {
     static displayBooks = () => {
-        const storedBooks = BooksStoreUtilities.getBooks();
-        cardContainer.innerHTML = '';
-        let cardElement = '';
-  
-        for (let i = 0; i < storedBooks.length; i += 1) {
-          cardElement += `<div class="card">
+      const storedBooks = BooksStoreUtilities.getBooks();
+      cardContainer.innerHTML = '';
+      let cardElement = '';
+
+      for (let i = 0; i < storedBooks.length; i += 1) {
+        cardElement += `<div class="card">
                   <h2 class="content">${storedBooks[i].title}, </h2>
                   <button class='remove' value="${storedBooks[i].title}" type="button"> Remove</button>
               </div> <hr>`;
-        }
-        cardContainer.innerHTML = cardElement;
       }
+      cardContainer.innerHTML = cardElement;
+    }
 }
 
 // add book
@@ -41,6 +43,8 @@ document.getElementById('addbtn').addEventListener('click', () => {
   const oldBooks = BooksStoreUtilities.getBooks();
   BooksStoreUtilities.addBook(oldBooks);
   MainUI.displayBooks();
+  titleEl.value = '';
+  authorEl.value = '';
 });
 
 // remove a book
@@ -53,5 +57,5 @@ document.getElementById('books').addEventListener('click', (event) => {
 
 //  get all books
 window.addEventListener('load', () => {
-    MainUI.displayBooks();
+  MainUI.displayBooks();
 });
