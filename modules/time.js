cc2972f6233c896ab84e18fe19f37bd2188fc2ef
Yannick <91213045+YannickZahinda@users.noreply.github.com>
2022-03-29 12:24:49 +0200

    fixed linter error

--- modules/time.js
+++ modules/time.js
@@ -1,13 +1,10 @@
-import {time} from './nav.js';
+import { time } from './nav.js';
 import { DateTime } from './luxon.js';
 
 /* eslint-disable import/prefer-default-export */
 const myLocalDate = () => {
-    time.innerHTML = DateTime.now().toLocaleString(
-      DateTime.DATETIME_MED_WITH_SECONDS,
-    );
-  };
-export {myLocalDate};  
-
-// const myLuxonDate = DateTime.now().toLocaleString(DateTime.DATETIME_FULL_WITH_SECONDS);
-// console.log(myLuxonDate)
+  time.innerHTML = DateTime.now().toLocaleString(
+    DateTime.DATETIME_MED_WITH_SECONDS
+  );
+};
+export { myLocalDate };
